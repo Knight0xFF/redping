6f5d745047452d1186b29de6999276c6930e503b
曾强 <937791848@qq.com>
2018-09-16 22:26:26 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pcap
+import dpkt
+import socket
+import struct
+from scapy.all import sniff
+import curses
+
+#
+# pc = pcap.pcap("en0")
+# # pc.setfilter("en0")
+# decode = { pcap.DLT_LOOP: dpkt.loopback.Loopback,
+#            pcap.DLT_NULL: dpkt.loopback.Loopback,
+#            pcap.DLT_EN10MB: dpkt.ethernet.Ethernet }[pc.datalink()]
+# try:
+#     print 'listening on %s: %s' % (pc.name, pc.filter)
+#     for ts, pkt in pc:
+#         print ts, `decode(pkt)`
+# except KeyboardInterrupt:
+#     nrecv, ndrop, nifdrop = pc.stats()
+#     print '\n%d packets received by filter' % nrecv
+#     print '%d packets dropped by kernel' % ndrop
+
+sniff(filter="", iface="en0", prn=lambda x: x.show())
